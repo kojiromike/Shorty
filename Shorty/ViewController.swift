fac634398c1512540ba2407678a0061139379fab
Michael A. Smith <msmith3@ebay.com>
2015-08-30 21:11:24 -0400

    Initial Commit

---
+++ Shorty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shorty
+//
+//  Created by Michael Smith on 8/30/15.
+//  Copyright (c) 2015 Michael Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
