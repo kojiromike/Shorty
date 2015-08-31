e342c83acebc8cce905e1a4304e8f84791c99ee1
Michael A. Smith <msmith3@ebay.com>
2015-08-30 22:23:11 -0400

    Basic Web Viewer

--- Shorty/ViewController.swift
+++ Shorty/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var urlField: UITextField!
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,20 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loadLocation(AnyObject) {
+        var urlText = urlField.text
+        
+        if !urlText.hasPrefix("http:") && !urlText.hasPrefix("https:") {
+            if !urlText.hasPrefix("//") {
+                urlText = "//" + urlText
+            }
+            urlText = "https:" + urlText
+        }
+        
+        let url = NSURL(string: urlText)!
+        webView.loadRequest(NSURLRequest(URL: url))
     }
 
 
